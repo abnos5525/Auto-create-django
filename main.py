75e900be4f79fe5085f5730bad04c0311ba56fea
Hossein <87528394+abnos5525@users.noreply.github.com>
2023-07-22 13:26:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import subprocess
 # Open CMD
 cmd_process = subprocess.Popen("cmd", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-time.sleep(3)
+time.sleep(5)
 
 # creating virtual environment named env
 gu.typewrite('python -m venv env')
 gu.hotkey('enter')
 
+time.sleep(2)
 # activate virtual environment
 gu.typewrite('cd env/scripts')
 gu.hotkey('enter')
@@ -21,6 +22,7 @@ gu.hotkey('enter')
 gu.typewrite('pip install django')
 gu.hotkey('enter')
 
+time.sleep(3)
 # getting back to root directory
 gu.typewrite('cd ..')
 gu.hotkey('enter')
@@ -37,6 +39,7 @@ gu.hotkey('enter')
 gu.typewrite('django-admin startproject config .')
 gu.hotkey('enter')
 
+time.sleep(2)
 # migrating 
 gu.typewrite('python manage.py migrate')
 gu.hotkey('enter')
